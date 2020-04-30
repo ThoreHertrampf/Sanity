e8d66f6428e5b5ef477461af7578758cb6951bea
Thore Hertrampf <thorehertrampf@live.com>
2020-04-30 14:49:36 +0200

    implemented gui for create template

--- main.py
+++ main.py
@@ -8,22 +8,93 @@ from PIL import ImageTk, Image
 
 def create_template():
 
+    def confirm_name():
+
+        def confirm_template():
+
+            def yes():
+                new_template = Template(name, text)
+                template_file = open("Templates.txt", "a+")
+                template_file.write("\n\n" + new_template.name + "\n\n" + new_template.text + "\n" + "End of template: " + new_template.name + "\n")
+                template_file.close()
+                create_window.destroy()
+
+            def no():
+                template.destroy()
+                question.destroy()
+                yes.destroy()
+                no.destroy()
+                cancel.destroy()
+                return
+
+            def cancel():
+                create_window.destroy()
+
+
+            text = pyperclip.paste()
+            question = Label(create_window, text = "Is this the input you want to save as a template?")
+            question.grid(row = 3, column = 2)
+            template = Label(create_window, text = text)
+            template.grid(row = 4, column = 2)
+
+
+            yes = Button(create_window, text = "Yes", command = yes)
+            yes.grid(row = 5, column = 1)
+            no = Button(create_window, text = "No", command = no)
+            no.grid(row = 5, column = 2)
+            cancel = Button(create_window, text = "Cancel", command = cancel)
+            cancel.grid(row = 5, column = 3)
+
+
+        name = template_name.get()
+        template_name.delete(0, END)
+
+        template_file = open("Templates.txt", "r")
+        lines_file = template_file.readlines()
+
+        for lines in lines_file:
+            while lines == name+"\n":
+                global wrong_name
+                wrong_name = Label(create_window, text = "This name is already in use, please choose another one")
+                wrong_name.grid(row = 2, column = 2)
+                return
+        template_file.close()
+        pyperclip.copy("")
+
+        if 'wrong_name' in globals():
+            wrong_name.destroy()
+
+        right_name = Label(create_window, text = "The name \"" + name + "\" is good!")
+        right_name.grid(row = 2, column = 2)
+
+        enter_template = Label(create_window, text = "Copy template to clipboard!")
+        enter_template.grid(row = 3, column = 1)
+
+        confirmation_template = Button(create_window, text = "Confirm!", command = confirm_template)
+        confirmation_template.grid(row = 3, column = 3)
+
+
+
+
     create_window = Toplevel()
     create_window.title("Create Template")
     create_window.geometry("1000x600")
 
+    label_entry = Label(create_window, text = "Enter the name of a new template!")
+    label_entry.grid(row = 1, column = 1)
+
     template_name = Entry(create_window,  width = 40, borderwidth = 10)
-    template_name.grid(row = 1, column = 1)
-    template_name.delete(0, END)
-    template_name.insert(0, "Enter the name of a new template!")
+    template_name.grid(row = 1, column = 2)
 
-    confirm_button = Button(create_window, text = "Confirm", command = template_name.get)
-    confirm_button.grid(row = 1, column = 2)
-    
+    confirm_button = Button(create_window, text = "Confirm!", command = confirm_name)
+    confirm_button.grid(row = 1, column = 3)
 
 
 
-    template_file = open("Templates.txt", "r")
+
+
+
+    '''template_file = open("Templates.txt", "r")
     lines_file = template_file.readlines()
     name = input("Enter the name of a new template: ")
 
@@ -51,9 +122,9 @@ def create_template():
         elif confirmation == "No" or confirmation == "no":
             continue
         elif confirmation == "Exit" or confirmation == "exit":
-            break
+            break'''
 
-    create_window.destroy()
+
 
 def change_template():
     print("Changed Template")
